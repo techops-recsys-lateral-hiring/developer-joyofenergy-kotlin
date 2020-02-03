bccec51f54c8ba8d6134927f63e0431dc9aa24ca
Mario Fernandez <mario.fernandez@hceris.com>
2020-02-03 23:03:08 +0100

    #MF replace assertions with strikt

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -4,16 +4,19 @@ import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
     fun `tests root`() {
         withTestApplication({ module(testing = true) }) {
             handleRequest(HttpMethod.Get, "/").apply {
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("HELLO WORLD!", response.content)
+                expectThat(response) {
+                    get { status() }.isEqualTo(HttpStatusCode.OK)
+                    get { content }.isEqualTo("HELLO WORLD!")
+                }
             }
         }
     }
