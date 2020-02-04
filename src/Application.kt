2f7c89254f84e178a6cb1077276f8a5be894d542
Mario Fernandez <mario.fernandez@hceris.com>
2020-02-04 16:08:52 +0100

    1893 #MF activate routes for calculating costs

--- src/Application.kt
+++ src/Application.kt
@@ -4,10 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import de.tw.energy.controllers.MeterReadingController
+import de.tw.energy.controllers.PricePlanComparatorController
 import de.tw.energy.domain.MeterReadings
+import de.tw.energy.domain.PricePlan
 import de.tw.energy.domain.Response
 import de.tw.energy.domain.ResponseWithBody
+import de.tw.energy.services.AccountService
 import de.tw.energy.services.MeterReadingService
+import de.tw.energy.services.PricePlanService
 import io.ktor.application.Application
 import io.ktor.application.ApplicationCall
 import io.ktor.application.call
@@ -20,6 +24,7 @@ import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.route
 import io.ktor.routing.routing
+import java.math.BigDecimal
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -38,6 +43,20 @@ suspend inline fun <T> ApplicationCall.respond(r: Response<T>) {
     }
 }
 
+private const val DR_EVILS_DARK_ENERGY_ENERGY_SUPPLIER = "Dr Evil's Dark Energy"
+private const val THE_GREEN_ECO_ENERGY_SUPPLIER = "The Green Eco"
+private const val POWER_FOR_EVERYONE_ENERGY_SUPPLIER = "Power for Everyone"
+
+private const val MOST_EVIL_PRICE_PLAN_ID = "price-plan-0"
+private const val RENEWABLES_PRICE_PLAN_ID = "price-plan-1"
+private const val STANDARD_PRICE_PLAN_ID = "price-plan-2"
+
+private const val SARAHS_SMART_METER_ID = "smart-meter-0"
+private const val PETERS_SMART_METER_ID = "smart-meter-1"
+private const val CHARLIES_SMART_METER_ID = "smart-meter-2"
+private const val ANDREAS_SMART_METER_ID = "smart-meter-3"
+private const val ALEXS_SMART_METER_ID = "smart-meter-4"
+
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
@@ -48,6 +67,24 @@ fun Application.module(testing: Boolean = false) {
     }
 
     val meterReadingsService = MeterReadingService(mutableMapOf())
+    val pricePlanService = PricePlanService(
+        listOf(
+            PricePlan(MOST_EVIL_PRICE_PLAN_ID, DR_EVILS_DARK_ENERGY_ENERGY_SUPPLIER, BigDecimal.TEN, listOf()),
+            PricePlan(MOST_EVIL_PRICE_PLAN_ID, DR_EVILS_DARK_ENERGY_ENERGY_SUPPLIER, BigDecimal.TEN, listOf()),
+            PricePlan(STANDARD_PRICE_PLAN_ID, POWER_FOR_EVERYONE_ENERGY_SUPPLIER, BigDecimal.ONE, listOf())
+
+        ),
+        meterReadingsService
+    )
+    val accountService = AccountService(
+        mapOf(
+            SARAHS_SMART_METER_ID to MOST_EVIL_PRICE_PLAN_ID,
+            PETERS_SMART_METER_ID to RENEWABLES_PRICE_PLAN_ID,
+            CHARLIES_SMART_METER_ID to MOST_EVIL_PRICE_PLAN_ID,
+            ANDREAS_SMART_METER_ID to STANDARD_PRICE_PLAN_ID,
+            ALEXS_SMART_METER_ID to RENEWABLES_PRICE_PLAN_ID
+        )
+    )
 
     routing {
         route("/readings") {
@@ -62,6 +99,19 @@ fun Application.module(testing: Boolean = false) {
             post("/store") {
                 val readings = call.receive<MeterReadings>()
                 val response = controller.storeReadings(readings)
+                call.respond(response)
+            }
+        }
+
+        route("/price-plans") {
+            val controller = PricePlanComparatorController(
+                pricePlanService,
+                accountService
+            )
+
+            get("/compare-all/{smartMeterId}") {
+                val smartMeterId = call.parameters["smartMeterId"] ?: ""
+                val response = controller.calculatedCostForEachPricePlan(smartMeterId)
                 call.respond(response)
             }
         }
--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -5,6 +5,7 @@ import de.tw.energy.domain.MeterReadings
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.TestApplicationEngine
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.setBody
 import io.ktor.server.testing.withTestApplication
@@ -35,12 +36,7 @@ class ApplicationTest {
     @Test
     fun `retrieves readings`() {
         withTestApplication({ module(testing = true) }) {
-            handleRequest(HttpMethod.Post, "/readings/store") {
-                addHeader(HttpHeaders.Accept, "application/json")
-                addHeader(HttpHeaders.ContentType, "application/json")
-                setBody(MeterReadings.generate("smart-meter-1").toJson())
-
-            }
+            populateReadings()
 
             handleRequest(HttpMethod.Get, "/readings/read/smart-meter-1") {
                 addHeader(HttpHeaders.Accept, "application/json")
@@ -49,6 +45,29 @@ class ApplicationTest {
                 expectThat(response)
                     .get { status() }.isEqualTo(HttpStatusCode.OK)
             }
+        }
+    }
+
+    @Test
+    fun `compares prices`() {
+        withTestApplication({ module(testing = true) }) {
+            populateReadings()
+
+            handleRequest(HttpMethod.Get, "/price-plans/compare-all/smart-meter-1") {
+                addHeader(HttpHeaders.Accept, "application/json")
+            }.apply {
+                expectThat(response)
+                    .get { status() }.isEqualTo(HttpStatusCode.OK)
+            }
+        }
+    }
+
+    private fun TestApplicationEngine.populateReadings() {
+        handleRequest(HttpMethod.Post, "/readings/store") {
+            addHeader(HttpHeaders.Accept, "application/json")
+            addHeader(HttpHeaders.ContentType, "application/json")
+            setBody(MeterReadings.generate("smart-meter-1").toJson())
+
         }
     }
 }
